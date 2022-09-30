43fafba51294120c5e5b62b839a4f0241de8a24c
Rodrigo <rodrigosedano@me.com>
2022-10-01 01:06:47 +0200

    short text

--- pages/WelcomePage.sh
+++ pages/WelcomePage.sh
@@ -17,8 +17,8 @@ clear
 while true; do
 	question=$(whiptail --title "EmuDeck configuration mode" \
    --radiolist "Move using your DPAD and select your platforms with the Y button. Press the A button to select." 10 80 4 \
-	"EASY" "This is a 100% automatic mode. We will configure\n your device with the recommended settings so you can start playing right away." OFF \
-	"CUSTOM" "This mode gives you a bit more of control on how EmuDeck\n configures your system. You will be able to configure Aspect Ratios, Bezels, Filters, RetroAchievments, Emulators, etc." OFF \
+	"EASY" "This is a 100% automatic mode. Install & Play" OFF \
+	"CUSTOM" "You will be able to configure Aspect Ratios, Bezels, Filters, RetroAchievments, Emulators, etc." OFF \
    3>&1 1<&2 2>&3)
 	case $question in
 		[EASY]* ) break;;
