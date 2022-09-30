bd6c9295c5f899d5b37397069d6476867ec3ba8f
Rodrigo <rodrigosedano@me.com>
2022-09-30 21:23:58 +0200

    RomStorage

--- pages/RomStoragePage.sh
+++ pages/RomStoragePage.sh
@@ -1,19 +1,41 @@
 #!/bin/sh
-while true; do
-	question=$(whiptail --title "Choose your Storage" \
-   --radiolist "Where do you want to store your roms? " 10 80 4 \
-	"INTERNAL" "We will create your rom folders on your Android's Internal Storage" OFF \
-	"SDCARD" "If your device has a SDCARD " OFF \
-   3>&1 1<&2 2>&3)
-	case $question in
-		[EASY]* ) break;;
-		[CUSTOM]* ) break;;
-		* ) echo "Please answer yes or no.";;
-	esac
-done
 
-if [ $question == 'EASY' ]; then
-	setSetting expert false
+if [ $hasSDCARD == true ]; then
+	
+	while true; do
+		question=$(whiptail --title "Choose your Storage" \
+   	--radiolist "Where do you want to store your roms? " 10 80 4 \
+		"INTERNAL" "We will create your rom folders on your Android's Internal Storage" OFF \
+		"SDCARD" "If your device has a SDCARD " OFF \
+   	3>&1 1<&2 2>&3)
+		case $question in
+			[INTERNAL]* ) break;;
+			[SDCARD]* ) break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+	
+	if [ $question == 'INTERNAL' ]; then
+		PATH=$HOME
+	else		
+		SDPath=$(readlink ~/storage/external-1)
+		firstString=$SDPath
+		secondString=""
+		sdcardID="${firstString/"/storage/"/"$secondString"}"     
+		firstString=$sdcardID
+		secondString=""
+		sdcardID="${firstString/"/Android/data/com.termux/files"/"$secondString"}" 
+		PATH=$sdcardID
+	fi
+	
+
 else
-	setSetting expert true
+	PATH=$HOME
 fi
+
+setSetting emulationPath $PATH/Emulation
+setSetting romsPath $PATH/Emulation/roms
+setSetting toolsPath $PATH/Emulation/tools
+setSetting biosPath $PATH/Emulation/bios
+setSetting savesPath $PATH/Emulation/saves
+setSetting storagePath $PATH/Emulation/storage
