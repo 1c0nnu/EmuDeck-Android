75e52212dcbeb8083fb848c8fe7914f25ba627fd
Rodrigo <rodrigosedano@me.com>
2022-10-01 01:19:17 +0200

    switch

--- pages/EmulatorSelectorPage.sh
+++ pages/EmulatorSelectorPage.sh
@@ -2,7 +2,7 @@
 
 case $devicePower in
 "0")	
-	while true; do
+	# while true; do
 		question=$(whiptail --title "Emulators" \
    	--checklist "These are the emulators you can install. We've disabled those not recommended for your device. You can disable more emulators if you don't want to install all of them" 10 80 4 \
 		"RA" "RetroArch - Classic 2D and 3D Games" ON \
@@ -13,20 +13,20 @@ case $devicePower in
 		"PPSSPP" "PPSSPP - Sony PSP" ON \
 		"SKYLINE" "PPSSPP - Nintendo Switch" OFF \
    	3>&1 1<&2 2>&3)
-		case $question in
-			[RA]* ) break;;
-			[DOLPHIN]* ) break;;
-			[DUCK]* ) break;;
-			[AETHERSX2]* ) break;;
-			[CITRA]* ) break;;
-			[PPSSPP]* ) break;;
-			[SKYLINE]* ) break;;
-			* ) echo "Please select your emus 0.";;
-		esac
-	done
+	# 	case $question in
+	# 		[RA]* ) break;;
+	# 		[DOLPHIN]* ) break;;
+	# 		[DUCK]* ) break;;
+	# 		[AETHERSX2]* ) break;;
+	# 		[CITRA]* ) break;;
+	# 		[PPSSPP]* ) break;;
+	# 		[SKYLINE]* ) break;;
+	# 		* ) echo "Please select your emus 0.";;
+	# 	esac
+	# done
 ;;
 "1")	
-	while true; do
+	# while true; do
 		question=$(whiptail --title "Emulators" \
 	   --checklist "These are the emulators you can install. We've disabled those not recommended for your device. You can disable more emulators if you don't want to install all of them" 10 80 4 \
 		"RA" "RetroArch - Classic 2D and 3D Games" ON \
@@ -37,20 +37,20 @@ case $devicePower in
 		"PPSSPP" "PPSSPP - Sony PSP" ON \
 		"SKYLINE" "PPSSPP - Nintendo Switch" OFF \
    	3>&1 1<&2 2>&3)
-		case $question in
-			[RA]* ) break;;
-			[DOLPHIN]* ) break;;
-			[DUCK]* ) break;;
-			[AETHERSX2]* ) break;;
-			[CITRA]* ) break;;
-			[PPSSPP]* ) break;;
-			[SKYLINE]* ) break;;
-			* ) echo "Please select your emus 1.";;
-		esac
-	done
+	# 	case $question in
+	# 		[RA]* ) break;;
+	# 		[DOLPHIN]* ) break;;
+	# 		[DUCK]* ) break;;
+	# 		[AETHERSX2]* ) break;;
+	# 		[CITRA]* ) break;;
+	# 		[PPSSPP]* ) break;;
+	# 		[SKYLINE]* ) break;;
+	# 		* ) echo "Please select your emus 1.";;
+	# 	esac
+	# done
 ;;
 "2")
-	while true; do
+	# while true; do
 		question=$(whiptail --title "Emulators" \
 	   --checklist "These are the emulators you can install. We've disabled those not recommended for your device. You can disable more emulators if you don't want to install all of them" 10 80 4 \
 		"RA" "RetroArch - Classic 2D and 3D Games" ON \
@@ -61,17 +61,17 @@ case $devicePower in
 		"PPSSPP" "PPSSPP - Sony PSP" ON \
 		"SKYLINE" "PPSSPP - Nintendo Switch" ON \
    	3>&1 1<&2 2>&3)
-		case $question in
-			[RA]* ) break;;
-			[DOLPHIN]* ) break;;
-			[DUCK]* ) break;;
-			[AETHERSX2]* ) break;;
-			[CITRA]* ) break;;
-			[PPSSPP]* ) break;;
-			[SKYLINE]* ) break;;
-			* ) echo "Please select your emus 2.";;
-		esac
-	done	
+	# 	case $question in
+	# 		[RA]* ) break;;
+	# 		[DOLPHIN]* ) break;;
+	# 		[DUCK]* ) break;;
+	# 		[AETHERSX2]* ) break;;
+	# 		[CITRA]* ) break;;
+	# 		[PPSSPP]* ) break;;
+	# 		[SKYLINE]* ) break;;
+	# 		* ) echo "Please select your emus 2.";;
+	# 	esac
+	# done	
 ;;
 *)
 	echo "default"
