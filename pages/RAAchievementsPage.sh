95fc4b1c00f4dd99c23266785362c4036329912c
Rodrigo <rodrigosedano@me.com>
2022-10-01 03:15:36 +0200

    fixes

--- pages/RAAchievementsPage.sh
+++ pages/RAAchievementsPage.sh
@@ -2,18 +2,18 @@
 
 if [ $android -lt 11 ]; then
 	question=$(whiptail --title "RetroAchievments" \
-	--inputbox "Do you want to configure RetroAchievments? Insert your username..." 10 80 4 \
+	--inputbox "Do you want to configure RetroAchievments? Insert your username..." 10 80 \
 	3>&1 1<&2 2>&3)
 	status=$?
 	
 	if [ $status = 0 ]; then
 		setSetting achievementsUser $question
 		question=$(whiptail --title "RetroAchievments" \
-		--inputbox "...and your password" 10 80 4 \
+		--inputbox "...and your password" 10 80 \
 		3>&1 1<&2 2>&3)
 		status=$?
 		if [ $status = 0 ]; then
-			setSetting achievementsUser $question
+			setSetting achievementsPass $question
 		fi
 		
 	fi
