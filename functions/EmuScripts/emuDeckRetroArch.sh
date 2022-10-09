bfccf070b369b23698e87ca915b2e4298bf01ea5
Rodrigo <rodrigosedano@me.com>
2022-10-09 18:23:17 +0200

    FIX FF

--- functions/EmuScripts/emuDeckRetroArch.sh
+++ functions/EmuScripts/emuDeckRetroArch.sh
@@ -53,7 +53,7 @@ RetroArch_init(){
 		cp -vp "$backupfile" "$backupfile.bak"
 						
 		updateOrAppendConfigLine "$backupfile" "input_enable_hotkey_btn =" 'input_enable_hotkey_btn = "109"'
-		updateOrAppendConfigLine "$backupfile" "input_hold_fast_forward_axis =" 'input_hold_fast_forward_axis = "+9"'
+		updateOrAppendConfigLine "$backupfile" "input_hold_fast_forward_btn =" 'input_hold_fast_forward_btn = "105"'
 		updateOrAppendConfigLine "$backupfile" "input_load_state_btn =" 'input_load_state_btn = "102"'
 		updateOrAppendConfigLine "$backupfile" "input_menu_toggle_gamepad_combo =" 'input_menu_toggle_gamepad_combo = "2"'
 		updateOrAppendConfigLine "$backupfile" "input_quit_gamepad_combo =" 'input_quit_gamepad_combo = "4"'
