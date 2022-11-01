7a409467f27f576b2ebf0b05475bb614234e84c3
Rodrigo <rodrigosedano@me.com>
2022-11-01 11:47:31 +0100

    Revert "Pegasus off by default"
    This reverts commit fed58c81e7e86eaab70055ff8605cca9a6db984d.

--- settings.sh
+++ settings.sh
@@ -56,7 +56,7 @@ RAHandClassic2D=false
 RAHandHeldShader=true
 doSetupSaveSync=false
 
-doInstallPegasus=false
+doInstallPegasus=true
 doInstallDaijisho=true
 doInstallDig=false
 doInstallLaunchbox=false
